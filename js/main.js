d4e36e1e0d3242c5f7894adbfa70b5afa4f05555
vludwick <victor_ludwick@hotmail.com.br>
2019-07-09 15:23:04 -0300

    Adicionada segunda página

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-    $("#login").submit(function(){
-      alert("Login e/ou senha incorreta");
+  
+    $("#login").submit(function(event){
+      if($("#user").val() == "nanessinha" && $("#password").val() == "10/04"){
+        alert("Te amo");
+        event.preventDefault();
+        $("#link").html("<a href='/login2.html'>Clique aqui para seguir para a outra página</a>");
+        
+      }
+      else{
+        alert("Login e/ou senha incorreta");
+      }
     });
   });
