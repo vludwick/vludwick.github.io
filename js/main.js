d34385a678bec421077be7c87887218e0653c23a
victor.ludwick <victor.domingues@vm2.com.br>
2019-10-07 16:34:46 -0300

    reboot

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
 $(document).ready(function(){
-  
-    $("#login").submit(function(event){
-      if($("#user").val() == "nanessinha" && $("#password").val() == "10/04"){
-        alert("Te amo");
-        event.preventDefault();
-        $("#link").html("<a href='/login2.html'>Clique aqui para seguir para a outra página</a>");
-        
-      }
-      else{
-        alert("Login e/ou senha incorreta");
-      }
-    });
+
   });
